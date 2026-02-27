dc393d40630a5cc6cc8e646903a22ef9974dcd6f
Liyanage J. Amila Prasad Perera <designitaltelo@gmail.com>
2026-02-27 19:06:25 +0100

    chore: remove unused datastore dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -105,7 +105,6 @@ dependencies {
   kapt("androidx.room:room-compiler:2.8.4")
   implementation("androidx.room:room-ktx:2.8.4")
 
-  implementation("androidx.datastore:datastore-preferences:1.1.1")
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
   implementation("com.jakewharton.timber:timber:5.0.1")
